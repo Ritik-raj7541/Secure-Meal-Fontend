4d6021b5fa5de4e9a080736d8f55dceda38122d9
a4aditya7 <adityamarandi36@gmail.com>
2023-11-30 22:44:00 +0530

    added logout in navbar

--- src/components/CouponSlider.jsx
+++ src/components/CouponSlider.jsx
@@ -1,9 +1,9 @@
 import { useState, useRef, useEffect } from "react";
-import a from '../assets/pics/a.png' ;
+import a from '../assets/pics/a.png';
 import QRCode from 'react-qr-code';
 
 export default function ImageSlider({ images }) {
-    console.log("ritik- ",images);
+    console.log("ritik- ", images);
     const indicatorWidthPercent = images.length > 0 ? 100 / images.length : 100;
 
     const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
@@ -43,36 +43,40 @@ export default function ImageSlider({ images }) {
     }, []);
 
     return (
-        <div className="w-full -z-50 grid place-items-center ">
+        <div className="w-full -z-50 grid grid-cols-1 place-items-center ">
             {/* Slider */}
-            <div
-                ref={sliderRef}
-                className="w-full flex flex-row overflow-x-scroll snap-x snap-mandatory "
-                style={{
-                    paddingBottom: "15px",
-                    clipPath: "inset(0 0 15px 0)",
-                }}
-            >
-                {
-                 images.map((obj, index) => {
-                    return (
-                        <div key={obj.url} className="w-full flex-shrink-0 snap-start lg:h-1/4 lg:w-1/4 round-sm shadow-lg">
-                            {index}
-                            <img src={obj.url} />
-                            <div>{obj.meal}</div>
-                        </div>
-                    );
-                }) 
-                }
-            </div>
-
+            {
+                (images.length == 0) ?
+                    <div>
+                        Your Coupons are not generated!
+                    </div> :
+                    <div
+                        ref={sliderRef}
+                        className="w-full flex flex-row overflow-x-scroll snap-x snap-mandatory "
+                        style={{
+                            paddingBottom: "15px",
+                            clipPath: "inset(0 0 15px 0)",
+                        }}
+                    >
+                        {
+                            images.map((obj, index) => {
+                                return (
+                                    <div key={obj.url} className="w-full flex-shrink-0 snap-start lg:h-1/4 lg:w-1/4 round-sm shadow-lg">
+                                        {index}
+                                        <img className="justify-center" src={obj.url} />
+                                        <div>{obj.meal}</div>
+                                    </div>
+                                    // <div key={obj.url}>
+                                    //     contents
+                                    //     </div>
+                                );
+                            })
+                        }
+                        
+                    </div>
+            }
             {/* Scroll indicator */}
-            <div className="flex lg:hidden">
-                <div className={`h-4 w-4 rounded-full text-center mx-2 ${currentSlideIndex==1 ? "bg-blue-300" : "bg-gray-100"}`} />
-                <div className={`h-4 w-4 rounded-full text-center mx-2 ${currentSlideIndex==3 ? "bg-green-300" : "bg-gray-100"}`} />
-                <div className={`h-4 w-4 rounded-full text-center mx-2 ${currentSlideIndex==5 ? "bg-yellow-300" : "bg-gray-100"}`} />
-                <div className={`h-4 w-4 rounded-full text-center mx-2 ${currentSlideIndex==7 ? "bg-red-300" : "bg-gray-100"}`} />
-            </div>
+
             {/* <div>{currentSlideIndex}</div> */}
             {/* <div className="w-full h-0.5 relative bg-gray-300">
         <div
@@ -84,6 +88,13 @@ export default function ImageSlider({ images }) {
           }}
         />
       </div> */}
+
+<div className="flex lg:hidden">
+                            <div className={`h-4 w-4 rounded-full text-center mx-2 ${currentSlideIndex == 1 ? "bg-blue-300" : "bg-gray-100"}`} />
+                            <div className={`h-4 w-4 rounded-full text-center mx-2 ${currentSlideIndex == 3 ? "bg-green-300" : "bg-gray-100"}`} />
+                            <div className={`h-4 w-4 rounded-full text-center mx-2 ${currentSlideIndex == 5 ? "bg-yellow-300" : "bg-gray-100"}`} />
+                            <div className={`h-4 w-4 rounded-full text-center mx-2 ${currentSlideIndex == 7 ? "bg-red-300" : "bg-gray-100"}`} />
+                        </div>
         </div>
     );
 }
